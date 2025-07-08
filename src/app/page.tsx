a7babd7a831dbd947fa74b7aa3d9e7f2052e5d61
my-mendix <m.ali@boubyantakaful.com>
2025-07-08 10:09:12 +0300

    removed duplicates codes in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 
-import Header from '../components/Layouts/Header'
-import SubHeader from '@/components/Layouts/SubHeader';
-import Footer from '@/components/Layouts/Footer';
 import HeroSection from '@/components/HomepageBanner/HeroSection';
 // import WhyBoubyanSection from '@/components/Miscs/WhyBoubyanSection';
 import NewsSection from '@/components/NewsSection/NewsSection';
@@ -11,14 +8,12 @@ import AboutBtic from '@/components/StrapiTest/AboutBtic';
 export default function Home() {
   return (
     <main>
-      <Header />
-      <SubHeader />
+      
       <HeroSection />
       <ProductSlider />
       <AboutBtic />
       {/* <WhyBoubyanSection /> */}
-      <NewsSection />
-      <Footer />
+      <NewsSection />      
     </main>
   );
 }
